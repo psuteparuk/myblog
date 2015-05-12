6e31a52dc046855581586e26f702dfddf21f5868
Potcharapol Suteparuk <psuteparuk@rocketfuel.com>
2015-05-12 11:30:51 -0700

    Fix bookmark issues in categories and tags

--- about.md
+++ about.md
@@ -4,11 +4,9 @@ title: About
 permalink: /about/
 ---
 
-Some information about you!
+Hi there, this is Potchy, 23, from Thailand, although currently based in Palo Alto, CA.
 
-### More Information
-
-A place to include any other types of information that you'd like to include about yourself.
+### More to come ...
 
 ### Contact me
 
--- main.js
+++ main.js
@@ -2,20 +2,19 @@ $(document).ready(function() {
   /* Sidebar */
   $('#sidebar-toggle').click(function(e) {
     e.preventDefault();
-    if ($(this).hasClass('cross')) {
-      $(this).removeClass('cross');
-      $(this).addClass('plus');
+    $(this).toggleClass('cross');
+    $(this).toggleClass('plus');
+
+    if ($(this).hasClass('plus')) {
       $('.sidebar').animate({ left: '-=25%' });
-      $('.site-avatar-small').removeClass('hidden');
+      $('.site-avatar-small').animate({ width: '50px', height: '50px' }).removeClass('hidden');
       $('.right-panel').animate({ width: '100%', 'margin-left': 0 }, 350, 'linear', function() {
         $(this).removeClass('wrapper-right');
         $(this).removeAttr('style');
       }).css('overflow', 'visible');
-    } else if ($(this).hasClass('plus')) {
-      $(this).removeClass('plus');
-      $(this).addClass('cross');
+    } else if ($(this).hasClass('cross')) {
       $('.sidebar').animate({ left: '+=25%' });
-      $('.site-avatar-small').addClass('hidden');
+      $('.site-avatar-small').animate({ width: 0, height: 0 }).addClass('hidden');
       $('.right-panel').animate({ width: '75%', 'margin-left': '25%' }, 350, 'linear', function() {
         $(this).addClass('wrapper-right');
         $(this).removeAttr('style');
@@ -27,15 +26,23 @@ $(document).ready(function() {
   });
 
   /* Categories and Tags */
-  $('.category-panel a, .tag-panel a').click(function(e) {
-    e.preventDefault();
+  var switchTab = function($tab) {
     $('.category-panel a, .tag-panel a').removeClass('active');
-    $(this).addClass('active');
+    $tab.addClass('active');
 
     $('.tab-pane').hide();
-    var idArr = $(this).data('tabname').split('-');
-    idArr.pop();
-    var tabname = idArr.join('-');
-    $('.tab-pane[data-tabname="' + tabname + '"]').show();
+    var tabname = $tab.attr('name');
+    $('.tab-pane[name="' + tabname + '"]').show();
+  };
+
+  $('.category-panel a, .tag-panel a').click(function(e) {
+    e.preventDefault();
+    switchTab($(this));
   });
+
+  var bookmark = window.location.hash;
+  if (bookmark && bookmark[0] === '#') {
+    var aSelector = 'a[name="' + bookmark.substr(1) + '"]';
+    switchTab($('.category-panel ' + aSelector + ', .tag-panel ' + aSelector));
+  }
 });
