6d487e656aacc120d4c9aa3e4dd0965bfe61a9a5
Potcharapol Suteparuk <psuteparuk@rocketfuel.com>
2015-05-11 18:21:22 -0700

    Able to toggle sidebar

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
+  /* Sidebar */
+  var $wrapperRightList = $('.wrapper-right');
+  $('#sidebar-toggle').click(function(e) {
+    e.preventDefault();
+    if ($(this).hasClass('cross')) {
+      $(this).removeClass('cross');
+      $(this).addClass('plus');
+      $('.sidebar').animate({ left: '-=25%' });
+      $('.site-avatar-small').removeClass('hidden');
+      $wrapperRightList.animate({ width: '100%', 'margin-left': 0 }, 350, 'linear', function() {
+        $wrapperRightList.removeClass('wrapper-right');
+        $wrapperRightList.removeAttr('style');
+      });
+    } else if ($(this).hasClass('plus')) {
+      $(this).removeClass('plus');
+      $(this).addClass('cross');
+      $('.sidebar').animate({ left: '+=25%' });
+      $('.site-avatar-small').addClass('hidden');
+      $wrapperRightList.animate({ width: '75%', 'margin-left': '25%' }, 350, 'linear', function() {
+        $wrapperRightList.addClass('wrapper-right');
+        $wrapperRightList.removeAttr('style');
+      });
+    }
+  });
+
   /* Categories and Tags */
   $('.category-panel a').click(function(e) {
     e.preventDefault();
